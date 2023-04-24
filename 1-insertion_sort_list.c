a0c073b687e3326ab844cd18149c0664e64eeb22
Josh-dev69 <ebubejoshua69@gmail.com>
2023-04-24 09:17:48 -0700

    Bubble and insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,32 +8,32 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev, *temp;
+	listint_t *node = NULL, *temp = NULL;
 	
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	current = (*list)->next;
-	while (current != NULL)
+	node = *list;
+	node = node->next;
+	while (node)
 	{
-		prev = current->prev;
-		temp = current->next;
-		while (prev != NULL && prev->n > current->n)
+		while (node->prev && node->n < (node->prev)->n)
 		{
-			/* Swap the nodes */
-			prev->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = prev;
-			current->next = prev;
-			current->prev = prev->prev;
-			prev->prev = current;
-			if (current->prev == NULL)
-				*list = current;
-			else
-				current->prev->next = current;
-			/* Print the list after each swap */
+			temp = node;
+			if (node->next)
+				(node->next)->prev = temp->prev;
+			(node->prev)->next = temp->next;
+			node = node->prev;
+			temp->prev = node->prev;
+			temp->next = node;
+			if (node->prev)
+				(node->prev)->next = temp;
+			node->prev = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			/* Print the List */
 			print_list(*list);
-			prev = current->prev;
+			node = node->prev;
 		}
-		current = temp;
+		node = node->next;
 	}
 }
